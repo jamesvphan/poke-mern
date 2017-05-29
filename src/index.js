3270d00f6fe3e6c0fe3f3a76344a5b0fe305964d
James Phan <jamesvphan@gmail.com>
2017-05-29 18:56:45 -0400

    Add react-router-redux package

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom'
 import createHistory from 'history/createBrowserHistory'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { routerMiddleware } from 'react-router-redux'
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
@@ -14,11 +15,19 @@ import rootReducer from './reducers/rootReducer.js'
 // import './styles/App.css'
 
 const history = createHistory()
-// const rMiddleware =
+const rMiddleware = routerMiddleware(history)
+
+// const initialState = {
+//
+// }
+
 const store = createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
+  // initialState,
+  composeWithDevTools(applyMiddleware(thunk, rMiddleware))
 )
+
+
 
 ReactDOM.render(
   <Provider store={store}>
